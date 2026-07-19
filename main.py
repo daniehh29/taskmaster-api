dc4cfe07f20a31e7aeb6d147723bdaaedd4b4481
daniehh29 <daniehh29@gmail.com>
2026-07-19 22:03:08 +0200

    SQLAlchemy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -7,3 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
